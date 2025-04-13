4791dc8bd1b423aba81df2bca0723345fb121dc4
Kelly Kingston <okellykings220@gmail.com>
2025-04-13 04:49:34 +0100

    prod

--- src/components/lib/api.tsx
+++ src/components/lib/api.tsx
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const authToken = urlParams.get("authToken");
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://betpool.online/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
